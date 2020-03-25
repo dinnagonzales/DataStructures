348de3435b529d3826e472a4bee566af99de4230
Dinna Gonzales <dinnagonzales.05@gmail.com>
2020-03-25 14:38:39 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+function LinkedList(){
+  this.head = null;
+  this.tail = null;
+}
+
+function Node(value, next, prev){
+  this.value = value;
+  this.next = next;
+  this.prev = prev;
+}
+
+LinkedList.prototype.addToHead = function(value){
+  var newNode = new Node(value, this.head, null);
+  if(this.head) this.head.prev = newNode;
+  else this.tail = newNode;
+  
+  this.head = newNode;
+}
+
+var ll = new LinkedList();
+ll.addToHead(100);
+ll.addToHead(200);
+ll.addToHead(300);
+
+
+console.log(ll);
