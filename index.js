0b3e51e51f4768e1482f5feaeb8bcbe47f1cf707
Dinna Gonzales <dinnagonzales.05@gmail.com>
2020-03-25 14:51:03 +0000

    Add to Tail method

--- index.js
+++ index.js
@@ -17,9 +17,16 @@ LinkedList.prototype.addToHead = function(value){
   this.head = newNode;
 }
 
-var ll = new LinkedList();
-ll.addToHead(100);
-ll.addToHead(200);
-ll.addToHead(300);
+LinkedList.prototype.addToTail = function(value){
+  var newNode = new Node(value, null, this.tail);
+  if(this.tail) this.tail.next = newNode;
+  else this.head = newNode;
+  
+  this.tail = newNode;
+}
 
+var ll = new LinkedList();
+ll.addToTail(400);
+ll.addToTail(300);
+ll.addToTail(500);
 console.log(ll);
